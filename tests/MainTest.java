7d1bffa91cdc4766678479cbcf194671ef6e16eb
kuzmin <kuz_alex_konst@mail.ru>
2024-03-09 22:02:07 +0400

    little additional about sout test

--- tests/MainTest.java
+++ tests/MainTest.java
@@ -1,8 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    static ByteArrayOutputStream baos;
+    static PrintStream ps;
 
-    @org.junit.jupiter.api.Test
-    void main() {
+
+    @BeforeAll
+    static void preparing() {
+        baos = new ByteArrayOutputStream();
+        ps = new PrintStream(baos);
+        System.setOut(ps);
     }
+
+    @Test
+    void testSout() {  //but its usually not important for backend
+        System.out.print("test log");
+        String res = baos.toString();
+        Assertions.assertEquals("test log", res);
+    }
+
+
 }
